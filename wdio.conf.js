6f1d2af7b417b6e97c5c53499e454683ef492520
wahyuridho <saputroridho569@gmail.com>
2024-08-04 15:46:22 +0700

    ubah tags pada wdio.conf.js ke regresion agar menjalankan test regresi

--- wdio.conf.js
+++ wdio.conf.js
@@ -155,7 +155,7 @@ export const config = {
         // <boolean> fail if there are any undefined or pending steps
         strict: false,
         // <string> (expression) only execute the features or scenarios with tags matching the expression
-        tags: '',
+        tags: '@regression',
         // <number> timeout for step definitions
         timeout: 60000,
         // <boolean> Enable this config to treat undefined definitions as warnings.
